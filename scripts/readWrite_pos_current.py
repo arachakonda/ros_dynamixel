9e64771f4d009a885929537dd0a74355dde73bcb
arachakonda <ananthrachakonda@gmail.com>
2023-06-01 00:42:27 +0530

    first milestone commit

--- scripts/readWrite_pos_current.py
+++ scripts/readWrite_pos_current.py
@@ -29,6 +29,8 @@ ADDR_PRESENT_POS = 132
 LEN_PRESENT_POS = 4
 LEN_PRESENT_CURRENT = 2
 
+LEN_PRESENT_DATA = 10
+
 # Protocol version
 PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
 
@@ -50,8 +52,8 @@ CURRENT_LIMIT = 3000
 portHandler = PortHandler(DEVICENAME)
 packetHandler = PacketHandler(PROTOCOL_VERSION)
 
-groupBulkWrite = groupBulkWrite(portHandler, PROTOCOL_VERSION)
-groupBulkRead = groupBulkRead(portHandler, PROTOCOL_VERSION)
+groupBulkWrite = GroupBulkWrite(portHandler, packetHandler)
+groupBulkRead = GroupBulkRead(portHandler, packetHandler)
 
 def open_port(portHandler):
     # Open port
@@ -91,31 +93,45 @@ def add_param(groupBulkRead, id, param_addr, param_len):
         print("[ID:%03d] groupBulkRead addparam failed" % id)
         quit()
 
-def bulkRead(groupBulkRead, id, param_addr, param_len):
+def bulkRead(groupBulkRead, packetHandler):
     dxl_comm_result = groupBulkRead.txRxPacket()
     if dxl_comm_result != COMM_SUCCESS:
         print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
 
+def extractData(groupBulkRead, id, param_addr, param_len):
     # Check if groupbulkread data of Dynamixel#1 is available
     dxl_getdata_result = groupBulkRead.isAvailable(id, param_addr, param_len)
     if dxl_getdata_result != True:
         print("[ID:%03d] groupBulkRead getdata failed" % id)
         quit()
-
     dxl_data = groupBulkRead.getData(id, param_addr, param_len)
-    
+
     return dxl_data
+
+def read_posCurr(id, groupBulkRead):
+    pub = rospy.Publisher('position_current', posCurr, queue_size=10)
+    rospy.init_node('read_posCurr', anonymous=True)
+    rate = rospy.Rate(10)
+    poscurr = posCurr()
+    while not rospy.is_shutdown():
+        poscurr.id = id
+        bulkRead(groupBulkRead, packetHandler)
+        poscurr.position = extractData(groupBulkRead, id, ADDR_PRESENT_POS, LEN_PRESENT_POS)
+        poscurr.current = extractData(groupBulkRead, id, ADDR_PRESENT_CURRENT, LEN_PRESENT_CURRENT)
+        poscurr.time = int(rospy.get_time())
+        print("ID: %03d, Position: %04d, Current: %05d, Time: %d"%(poscurr.id, poscurr.position, poscurr.current, poscurr.time))
+        rate.sleep()
 
 def main():
     open_port(portHandler)
     set_baudrate(portHandler, BAUDRATE)
-    enable_torque(portHandler, DXL_ID)
-    add_param(groupBulkRead, DXL_ID, ADDR_PRESENT_POS, LEN_PRESENT_POS)
-    add_param(groupBulkRead, DXL_ID, ADDR_PRESENT_CURRENT, LEN_PRESENT_CURRENT)
-    rate = rospy.Rate(10)
-
-    while not rospy.is_shutdown:
-        print(bulkRead(groupBulkRead, DXL_ID, ADDR_PRESENT_POS, LEN_PRESENT_POS))
-        rate.sleep()
+    enable_torque(portHandler, packetHandler, DXL_ID)
+    add_param(groupBulkRead, DXL_ID, ADDR_PRESENT_CURRENT, LEN_PRESENT_DATA)
+    try:
+        read_posCurr(DXL_ID, groupBulkRead)
+    except rospy.ROSInterruptException:
+        pass
 
 
+if __name__=='__main__':
+    main()
